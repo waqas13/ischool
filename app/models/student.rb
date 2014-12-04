3d7286a4b9a1d2698feb9f779e97d214283f7818
Im Genius <im.genius.man@gmail.com>
2014-12-04 19:17:00 +0500

    payment by durations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,12 @@ class Student < ActiveRecord::Base
 	
 	def self.import(file)
 		begin
+		  abc=0
 		  CSV.foreach(file.path, headers: true) do |row|
 		    student = find_by_id(row["id"]) || new
+		    abc = student.paid
 		    student.attributes = row.to_hash.slice(*row.to_hash.keys)
+		    student.paid = abc + row["paid"]
 		    student.date=nil
 		    student.paid=0
 		    student.save!
