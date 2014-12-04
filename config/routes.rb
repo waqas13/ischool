ecfb33e49f4e87f26974cb026d013cf84e0aef2f
Im Genius <im.genius.man@gmail.com>
2014-12-04 12:19:24 +0500

    routes2

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :students, path: '', path_names: {index: "students"} do
+  resources :students do
     collection { post :import }
   end
 
