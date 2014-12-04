f9d6f007b4c79dc966f915c1763d14b6a02af1ee
Im Genius <im.genius.man@gmail.com>
2014-12-04 18:16:41 +0500

    scholastic final

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,17 @@
 class Student < ActiveRecord::Base
 	
 	def self.import(file)
-	  CSV.foreach(file.path, headers: true) do |row|
-	    student = find_by_id(row["id"]) || new
-	    student.attributes = row.to_hash.slice(*row.to_hash.keys)
-	    student.save!
-	  end
+		begin
+		  CSV.foreach(file.path, headers: true) do |row|
+		    student = find_by_id(row["id"]) || new
+		    student.attributes = row.to_hash.slice(*row.to_hash.keys)
+		    student.date=nil
+		    student.paid=0
+		    student.save!
+		  end
+		rescue => e
+	        Rails.logger.error { "Encountered an error" }
+      		"ERROR UPLOADING CSV"
+      	end
 	end
 end
