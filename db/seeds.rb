0e20f9b97e069995da968b7156f9baba86ce39ca
Genius Programmer <me.genius.programmer@gmail.com>
2015-03-12 15:39:43 +0500

    POS initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# user for school system (master branch on git)
+# User.create!(email: "admin@school.com", password: "120120", user_type: 0, password_confirmation: "120120")
+# User.create!(email: "schooladmin@school.com", password: "school", user_type: 1, password_confirmation: "school")
+# User.create!(email: "exams@school.com", password: "school", user_type: 2, password_confirmation: "school")
+# User.create!(email: "accounts@school.com", password: "school", user_type: 3, password_confirmation: "school")
 
-User.create!(email: "admin@school.com", password: "120120", user_type: 0, password_confirmation: "120120")
-User.create!(email: "schooladmin@school.com", password: "school", user_type: 1, password_confirmation: "school")
-User.create!(email: "exams@school.com", password: "school", user_type: 2, password_confirmation: "school")
-User.create!(email: "accounts@school.com", password: "school", user_type: 3, password_confirmation: "school")
 
-
+# user for optics POS system (pos branch on git)
+User.create!(email: "admin@optics.com", password: "optics", user_type: 0, password_confirmation: "optics")
+User.create!(email: "sale@optics.com", password: "optics", user_type: 1, password_confirmation: "optics")
 
